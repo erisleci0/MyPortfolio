b4e65a1df9811fbb9147f8defb570114f0ff5d8b
Eris Leci <lecieris@hotmail.com>
2022-11-13 19:44:03 +0100

    Add viewmodel that wraps skills and socials

---
+++ MyPortfolio/ViewModels/PortfolioDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MyPortfolio.Models;
+
+namespace MyPortfolio.ViewModels
+{
+    public class PortfolioDetailsViewModel
+    {
+        public List<Skills>? Skills { get; set; }
+        public List<Socials>? Socials { get; set; }
+    }
+}
